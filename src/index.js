9bee56382d594e9374cf929a2d9a0239125dc1d0
Yijo Yim <hwangwhooss@gmail.com>
2021-03-29 17:00:02 -0500

    add react components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-open-sans';
+import 'typeface-lobster';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
