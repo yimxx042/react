a4f1aabd50e236218a7cfb20d8c3443e1a47d059
Yijo Yim <hwangwhooss@gmail.com>
2021-04-01 15:38:39 -0500

    add about, contact, header and footer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-open-sans';
 import 'typeface-lobster';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css'
 import App from './App';
 
 ReactDOM.render(
